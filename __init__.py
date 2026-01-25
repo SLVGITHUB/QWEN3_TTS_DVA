3b64cd616db5214d8ad85224334fc8e96c506d6e
VyacheslavD <88125029+SLVGITHUB@users.noreply.github.com>
2026-01-25 18:03:30 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-try:
-    import comfy.utils
-except ImportError:
-    pass
-else:
-    from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-    __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
