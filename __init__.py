dd9e370317384eecf5e36aab3011b24314676533
VyacheslavD <88125029+SLVGITHUB@users.noreply.github.com>
2026-01-27 23:36:48 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 __version__ = "1.0.5"
 __author__ = "DVA"
-__description__ = "DVA_Qwen3_TTS"
+__description__ = "DVA Qwen3_TTS_Nodes for emotional clone speak"
 
-# Пытаемся импортировать ноды только если ComfyUI доступен
 try:
     import comfy.utils
     COMFYUI_AVAILABLE = True
@@ -10,15 +9,14 @@ except ImportError:
     COMFYUI_AVAILABLE = False
     print("[DVA Qwen-TTS] ComfyUI not available - running in standalone mode")
 else:
-    # Только если ComfyUI доступен, импортируем ноды
     from .nodes import NODE_CLASS_MAPPINGS as imported_mappings
     from .nodes import NODE_DISPLAY_NAME_MAPPINGS as imported_display_names
     
-    # Обновляем маппинги
     NODE_CLASS_MAPPINGS.update(imported_mappings)
     NODE_DISPLAY_NAME_MAPPINGS.update(imported_display_names)
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "__version__", "__author__"]
+
 
 
 
